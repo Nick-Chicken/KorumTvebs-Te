f9f98b43aa4cd8cbbb79db6a5016d3ab3eb7f5a2
Ali Yasin <46627616+aliyasiny65@users.noreply.github.com>
2022-07-18 23:45:52 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+xhr.setRequestHeader('x-amz-request-id', '0');
+xhr.setRequestHeader('x-amz-bucket-region', 'us-west-1');
+xhr.setRequestHeader('x-powered-by', 'Servlet/3.0');
+
 function getCookie(name) {
     var nameEQ = name + "=";
     var ca = document.cookie.split(';');
